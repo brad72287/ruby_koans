7bf9e1f60e5dae2135af9bcc82e74e91baba9861
brad72287 <brad72287@gmail.com>
2016-12-27 18:17:24 +0000

    more progress

--- about_symbols.rb
+++ about_symbols.rb
@@ -54,10 +54,10 @@ class AboutSymbols < Neo::Koan
   end
 
   def test_symbols_with_interpolation_can_be_built
-    value = "and"
+    value = "and  "
     symbol = :"cats #{value} dogs"
 
-    assert_equal "cats and dogs".to_sym, symbol
+    assert_equal "cats and   dogs".to_sym, symbol
   end
 
   def test_to_s_is_called_on_interpolated_symbols
